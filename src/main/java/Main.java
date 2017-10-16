d1684ee4848ad8f1f059755f2787a8e8ac23a4d5
Joseph Brown <brownje@rose-hulman.edu>
2017-10-15 23:13:52 -0400

    Added addUser code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
+import edu.rosehulman.quota.javasdk.Product;
+import edu.rosehulman.quota.javasdk.QuotaService;
+import edu.rosehulman.quota.javasdk.User;
 
 public class Main {
 
-	public static void main(String[] args) {
-		//Quota quota = new Quota();
-
-	}
+  public static void main(String[] args) {
+    QuotaService quotaService = QuotaService.getReference("key");
+    Product product = quotaService.getProductById("1"); // or by name
+    product.addUser(new User("1"));
+  }
 
 }
